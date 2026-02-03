9b83c6da07158feb78c6ae7d2c0723e05ded0c2c
emmaegnot <bb23833@bristol.ac.uk>
2026-02-03 10:13:02 +0000

    hope it works

--- ping-pong.ino
+++ ping-pong.ino
@@ -28,23 +28,28 @@
 const int potentLeftPin = ;  // the number of the pushbutton pin
 const int potentRightPin = ;  // the number of the pushbutton pin
 
-const int resetPin = ;
+const int resetPin = D10;
 
-const int ledL1Pin = ;
-const int ledL2Pin = ;
+const int ledL1Pin = D5;
+const int ledL2Pin = D6;
 
-const int ledR1Pin = ;
-const int ledR2Pin = ;
+const int ledR1Pin = D1;
+const int ledR2Pin = D0;
+
+const int buzzer = D9;
+
+//TODO threshold for potentiometer
+const int threshold = ;
 
 // variables will change:
 int potentLState = 0;  // variable for reading the potentiometer status - will set a threshold for low/high
 int potentRState = 0;  // variable for reading the potentiometer status
+int resetState = 0;
 
 bool playing = false; // reset button sets this to true
 int winner = 0; // 0 is no winner, 1 is left, 2 is right
 bool canHit = false; //
 bool leftTurn = true;
-int randDelay = 0;
 
 // time variables
 unsigned long previousMillis = 0; // time the ball is hit
@@ -53,7 +58,7 @@ unsigned long timeDiff = 0;
 const long interval = 1000;  // interval at which to blink
 
 // flicker winner's light
-void hitToRight(int ledPin) {
+void flicker(int ledPin) {
     digitalWrite(ledPin, HIGH);
     delay(80);
     digitalWrite(ledPin, LOW);
@@ -68,11 +73,16 @@ void hitToLeft() {
   }
   else if (timeDiff < 400){
     digitalWrite(ledR1Pin, HIGH);
+    digitalWrite(ledR2Pin, LOW);
+
   } else if (timeDiff < 600) {
     // LED SCREEN THING
   } else if (timeDiff < 800) {
     digitalWrite(ledL2Pin, HIGH);
+    digitalWrite(ledR1Pin, LOW);
+
   } else {
+    digitalWrite(ledL2Pin, LOW);
     digitalWrite(ledL1Pin, HIGH);
     previousMillis = millis(); // reset timer
     canHit = true;
@@ -86,101 +96,100 @@ void hitToRight() {
     digitalWrite(ledL1Pin, HIGH);
   }
   else if (timeDiff < 400){
+    digitalWrite(ledL1Pin, LOW);
     digitalWrite(ledL2Pin, HIGH);
   } else if (timeDiff < 600) {
+    digitalWrite(ledL2Pin, LOW);
     // LED SCREEN THING
   } else if (timeDiff < 800) {
     digitalWrite(ledR1Pin, HIGH);
   } else {
     digitalWrite(ledR2Pin, HIGH);
+    digitalWrite(ledR1Pin, LOW);
     previousMillis = millis();
     canHit = true;
   }
 }
 
-// this function should check for the potentiometer being higher than the threshold within a time limit
-void hitBall(int potentiometerPin, unsigned long startTime){
-
-}
 
 
 void setup() {
   // initialize the LED pin as an output:
-  pinMode(ledLPin, OUTPUT);
-  pinMode(ledRPin, OUTPUT);
+  pinMode(ledL1Pin, OUTPUT);
+  pinMode(ledL2Pin, OUTPUT);
+  pinMode(ledR1Pin, OUTPUT);
+  pinMode(ledR2Pin, OUTPUT);
   // initialize the pushbutton pin as an input:
-  pinMode(buttonLPin, INPUT);
-  pinMode(buttonRPin, INPUT);
+  pinMode(resetPin, INPUT);
 }
 
 void loop() {
   // read the state of each potentiometer value:
-  buttonLState = digitalRead(buttonLPin);
-  buttonRState = digitalRead(buttonRPin);
+  potentLState = digitalRead(potentLeftPin);
+  potentRState = digitalRead(potentRightPin);
+  resetState = digitalRead(resetPin);
 
   // check if the game has been reset - change this to button
-  if (buttonLState == HIGH && buttonRState == HIGH) {
+  if (resetState == HIGH) {
       playing = true;
       winner = 0;
       delay(15);
-      digitalWrite(ledLPin, HIGH); //begin with left
+      leftTurn = true; //begin with left
   }
+
   while (playing){
     // get potentiometer values and threshold them
-    buttonLState = digitalRead(buttonLPin);
-    buttonRState = digitalRead(buttonRPin);
+    potentLState = digitalRead(potentLeftPin);
+    potentRState = digitalRead(potentRightPin);
 
 
     // check which turn - if left, call hitToLeft
     // this should only happen when canHit is false
-    if (leftTurn){
-      hitToLeft();
-
-    } else {
-      hitToRight();
-    }
-    
-    // once canHit is true, we need a new timer and detect if the hit has gone above the threshold
-    // the funtion hitBall should be used for this
-
-
-    // ALL OLD
-    randDelay = rand() % 1000 + 100;
-    // the game has two states: the left light is on or the right
-    if (light == 1) {
-
-      // if correct button is pressed, swap LEDs
-      if (buttonLState == HIGH) {
-        digitalWrite(ledLPin, LOW);
-        delay(randDelay);
-        digitalWrite(ledRPin, HIGH);
-        light = 2;
-        // if the wrong player presses a button, they lose and the game ends
-      } else if (buttonRState == HIGH) {
+    // if either potentiometer is high here, lose
+    if (!canHit){
+      if (potentLState > threshold){
+        playing = false;
+        winner = 2;
+      } else if (potentRState > threshold){
         playing = false;
         winner = 1;
       }
-
-    } else {
-      // if correct button is pressed, swap LEDs
-
-      if (buttonRState == HIGH) {
-        digitalWrite(ledRPin, LOW);
-        delay(randDelay);
-        digitalWrite(ledLPin, HIGH);
-        light = 1;
-      } else if (buttonLState == HIGH) {
-        playing = false;
-        winner = 2;
+      if (leftTurn){
+        hitToLeft();
+      } else {
+        hitToRight();
       }
+    } else {
+      // once canHit is true, we need a new timer and detect if the hit has gone above the threshold
+      // the funtion hitBall should be used for this
+      if (leftTurn && (potentLState > threshold)) {
+        // read left potentiometer and if higher than threshold 
+        canHit = false;
+        leftTurn = false;
+        previousMillis = millis(); //reset timer
 
+      } else if (!leftTurn && (potentRState > threshold)) {
+        canHit = false;
+        leftTurn = true;
+        previousMillis = millis();
+
+      }
     }
+    
+
 
   }
   // game over
+  //buzz
   if (winner == 1) {
-    flicker(ledLPin);
+    flicker(ledL1Pin);
   } else if (winner == 2) {
-    flicker(ledRPin);
+    flicker(ledR2Pin);
   }
+  if (buttonRState == HIGH) {
+    tone(buzzerPin, 100);
+    delay(200);
+    noTone(buzzerPin);
+    delay(200);
+}
 }
